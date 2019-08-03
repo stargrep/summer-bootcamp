95f2e35f0e9e32c62a4b9da133056d98e9f57adc
ericsummercamp <eric.j.summer.camp@gmail.com>
2019-08-03 14:13:13 -0700

    Add web crawler final proj

---
+++ final-project/web-crawler/src/module/HTML_fetcher.py
@@ -0,0 +1,5 @@
+# fetch web page by using a url link
+
+
+# this function should return text content of web page
+def fetch_web_page(url):
---
+++ final-project/web-crawler/src/module/URL_extractor.py
@@ -0,0 +1,4 @@
+
+# this function should extract url from web page. 
+# Notice that there's partial URL in the html page.
+def extract_url(web_page):
